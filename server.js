b37c743851293f98de50ac85b797eb07088d859b
JaceKace <jasonkitamura@gmail.com>
2020-03-31 13:01:53 -0400

    removing bcrypt code

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require( "express" );
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const bcrypt = require ( 'bcrypt' );
 require('dotenv').config()
 
 
